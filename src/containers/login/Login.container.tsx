8f3b64623b6d2e627c24f5fa989030a35de83d99
sergio <sergiosan2802@gmail.com>
2022-02-24 19:07:55 -0500

    feat: update container login

--- src/containers/login/Login.container.tsx
+++ src/containers/login/Login.container.tsx
@@ -1,21 +1,25 @@
 import { FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ButtonComponent from '../../components/button/Button.component';
 import './Login.container.css';
 
 export const LoginContainer = () => {
-  
+  const history = useNavigate();
+
+
   const handleSubmit = (event: FormEvent<HTMLElement>) => {
     event.preventDefault()
-    console.log('clic');
+    history('/crypto');
   }
 
-  
+
   return (
     <div className='login-container'>
 
       <form className='login-container__form' onSubmit={handleSubmit}>
         <input type="text" placeholder='ingrese su nombre' />
         <input type="password" placeholder='ingrese su password' />
-        <button  type='submit'> ingresar </button>
+        <ButtonComponent type='submit' children='Ingresar' />
       </form>
 
     </div>
---
+++ src/interfaces/button.interface.ts
@@ -0,0 +1,3 @@
+export interface buttonComponentInterface {
+  type?: 'submit' | 'reset' | 'button' | undefined;
+}
